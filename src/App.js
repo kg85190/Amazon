ea83880b532da84448aa6bd9f01a1aabd5531005
kg85190 <kg8519085687@gmail.com>
2023-12-24 16:59:25 +0530

    v1

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header></header>
+      <div className='a_main'></div>
+      <footer></footer>
+    </div>
+      
+  );
+}
+
+export default App;
